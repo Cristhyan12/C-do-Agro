f8942490aeb24d8ff444fa205108691ebdb47d78
Cristhyan12 <120782505+Cristhyan12@users.noreply.github.com>
2023-05-10 16:09:55 -0300

    C do Agro.cpp
    Fiz Alguma Alterações, mas ainda não estar na versão Final.

--- C do Agro.cpp
+++ C do Agro.cpp
@@ -1,22 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 //Programa em C que calcula Numeros de Arrobas de gado, para compra em venda de animais //
+// Autor: Júlio Cristhyan de Oliveira Toledo
+// Compilador: DevC++
+//  Data:10/05/2023
 int main(){
-int PB, AR, real;
+int PB, AR, f, real;
 
  printf("\t\t\t\t\t\tBem-Vindo\n");
- printf("\t\tInforme o peso Bruto do Animal:\t");
+ printf("\t\tInforme o peso Bruto do Novilhos:\t");
  scanf("%d",&PB);
  
  system("cls");
  
  AR = PB / 32,14;
  
- real = AR * 250;
+ real = AR * 300;
  
  printf("\t Contem %d Arrobas",AR);
  
  printf("\n\tO valor  das Arrobas e de %d R$Reais",real);
+ printf("\n\n\t\tInforme o peso Bruto da Femea:  ");
+ scanf("%d",&f);
+ system("cls");
+ 
+ AR = PB / 32,14;
+ f = AR * 250;
+ 
+ printf("\t Contem %d Arrobas",AR);
+ printf("\n\tO valor  das Arrobas das Novilhas e %d R$Reais",f);
+ printf("\n\n\n\n\t\t\t\tEspero Ter Ajudado :)");
 	return 0;
 }
 
